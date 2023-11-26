78c0e18c71ad7fc0abe51250b2dc537bcb557e13
Julian Ruiz Ocampo <124713530+JuliRuizOcampo@users.noreply.github.com>
2023-11-26 16:22:31 -0500

    1

---
+++ Solution/AddFriends/src/main/java/addfriends/AddFriends.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package addfriends;
+
+/**
+ *
+ * @author julir
+ */
+public class AddFriends {
+
+    public static void main(String[] args) {
+        Principal form = new Principal();
+        form.setVisible(true);
+    }
+}
